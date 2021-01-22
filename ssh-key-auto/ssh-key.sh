b0178a123c3fabffbdd6ba1fb1aa3a74bbcff4fb
arjunbnair97 <53293413+arjunbnair97@users.noreply.github.com>
2021-01-23 03:38:40 +0530

    Update ssh-key.sh

--- ssh-key-auto/ssh-key.sh
+++ ssh-key-auto/ssh-key.sh
@@ -4,5 +4,6 @@ for each_server in $(cat list.txt)
 do
   echo "Adding SSH Key"
   sshpass -f passd.txt ssh-copy-id ansadmin@${each_server}
+  sshpass -f passd.txt ssh-copy-id centos@${each_server}
 
 done
